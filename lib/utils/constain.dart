e2b69f91cb43cdff52849a5ea5831544b9608d64
RomjanHossain <romjanhossain726526@gmail.com>
2020-08-19 18:36:34 +0600

    added more const

--- lib/utils/constain.dart
+++ lib/utils/constain.dart
@@ -14,3 +14,28 @@ const ksmallText = TextStyle(
 const ktimeStyle = TextStyle(
   color: Colors.white,
 );
+
+const ksmt = TextStyle(
+  color: Colors.white,
+  fontSize: 25,
+);
+const kbigText = TextStyle(
+  color: Colors.white,
+  fontSize: 90,
+);
+const kfadetxt = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.w200,
+);
+
+const knxttxt = TextStyle(
+  color: Colors.lightBlueAccent,
+);
+const ksub = TextStyle(
+  fontSize: 12.0,
+  color: Colors.black54,
+);
+const ktitle = TextStyle(
+  fontSize: 25,
+  fontWeight: FontWeight.w500,
+);
