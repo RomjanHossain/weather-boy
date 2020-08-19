b5d72726eeed74337d138dcb04d3c2db833ac8eb
RomjanHossain <romjanhossain726526@gmail.com>
2020-08-19 15:58:13 +0600

    adding utils folder

---
+++ lib/utils/constain.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kboldText = TextStyle(
+  color: Colors.white,
+  fontSize: 25,
+  fontWeight: FontWeight.bold,
+);
+const ksmallText = TextStyle(
+  color: Colors.white,
+  fontSize: 11,
+  fontWeight: FontWeight.w300,
+);
+
+const ktimeStyle = TextStyle(
+  color: Colors.white,
+);
